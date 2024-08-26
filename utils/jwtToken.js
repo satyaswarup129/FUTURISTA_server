3b97788082fa5e323e72726606e6944efd08d20a
Satyaswarup Jena <148584629+satyaswarup129@users.noreply.github.com>
2024-08-26 22:15:34 +0530

    hihi

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -5,6 +5,7 @@ export const sendToken = (user, statusCode, message, res) => {
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ),
       httpOnly: true,
+      sameSite: 'strict'
     };
     res.status(statusCode).cookie("token", token, options).json({
       success: true,
